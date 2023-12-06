418abe0183945071f344e9a9aee531d5da1d90c6
Dmitry Shutov <dmitry.s@beda.software>
2023-12-06 12:47:36 +0700

    Export methods from instance module

--- src/services/index.ts
+++ src/services/index.ts
@@ -14,6 +14,7 @@ import {
     loading,
     notAsked,
 } from '../states'
+export * from './instance'
 
 export async function service<S = any, F = any>(config: AxiosRequestConfig): Promise<RemoteDataResult<S, F>> {
     try {
